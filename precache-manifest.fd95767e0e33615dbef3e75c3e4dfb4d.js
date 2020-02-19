77037300dcd7517aeed07c2d8fcfc41c9e20d8a6
Vitalii <narodnyjpikap@gmail.com>
2020-02-19 20:08:22 +0200

    first commit

---
+++ precache-manifest.fd95767e0e33615dbef3e75c3e4dfb4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a637fbf7517029cee65b0295f8f998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f3d67f71f56c5e4e310",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d3cd432b03f67a9d16e1",
+    "url": "/static/css/main.20cdf2cf.chunk.css"
+  },
+  {
+    "revision": "6f3d67f71f56c5e4e310",
+    "url": "/static/js/2.f7f29db1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f7f29db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cd432b03f67a9d16e1",
+    "url": "/static/js/main.bb1f91f7.chunk.js"
+  },
+  {
+    "revision": "2c87e160c5d16d3de555",
+    "url": "/static/js/runtime-main.d20077c2.js"
+  }
+]);
